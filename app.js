89e6c3b791a9b88ca3854a5a40a058cc41711336
Travless <travis.white0101@gmail.com>
2022-09-17 14:01:44 -0400

    create playRound() function that asks the user to input their selection of a choice between rock, paper, or scissors, runs the getComputerChoice function previously established, compares the two choices, and returns a winner by both alerting the player of the decision and logging it in the console. also return the scoring decision to either playerScore or computerScore variables.

--- app.js
+++ app.js
@@ -16,3 +16,44 @@ function getComputerChoice() {
 }
 // console.log(getComputerChoice());
 
+function playRound() {
+    let playersSelect = prompt('Please make your choice: Rock, Paper, or Scissors? ');
+    let computerSelection = getComputerChoice();
+    if (playersSelect === 'Rock' && computerSelection === 'Scissors') {
+        console.log(`Your opponent picked ${computerSelection}. You win! Rock beats Scissors!`);
+        alert(`Your opponent picked ${computerSelection}. You win! Rock beats Scissors!`);
+        playerScore += 1;
+        return playerScore;
+    } else if (playersSelect === 'Paper' && computerSelection === 'Rock') {
+        console.log(`Your opponent selected ${computerSelection}. You win! Paper beats Rock!`);
+        alert(`Your opponent selected ${computerSelection}. You win! Paper beats Rock!`);
+        playerScore += 1;
+        return playerScore;
+    } else if (playersSelect === 'Scissors' && computerSelection === 'Paper') {
+        console.log(`Your opponent selected ${computerSelection}. You win! Scissors beats Paper!`);
+        alert(`Your opponent selected ${computerSelection}. You win! Scissors beats Paper!`);
+        playerScore += 1;
+        return playerScore;
+    } else if (playersSelect === 'Scissors' && computerSelection === 'Rock') {
+        console.log(`Your opponent selected ${computerSelection}. You lose! Rock beats Scissors!`);
+        alert(`Your opponent selected ${computerSelection}. You lose! Rock beats Scissors!`);
+        computerScore += 1;
+        return computerScore;
+    } else if (playersSelect === 'Rock' && computerSelection === 'Paper') {
+        console.log(`Your opponent selected ${computerSelection}. You lose! Paper beats Rock!`);
+        alert(`Your opponent selected ${computerSelection}. You lose! Paper beats Rock!`);
+        computerScore += 1;
+        return computerScore;
+    } else if (playersSelect === 'Paper' && computerSelection === 'Scissors') {
+        console.log(`Your opponent selected ${computerSelection}. You lose! Scissors beats Paper!`);
+        alert(`Your opponent selected ${computerSelection}. You lose! Scissors beats Paper!`);
+        computerScore += 1;
+        return computerScore;
+    } else {
+        console.log(`Your opponent selected ${computerSelection} as well. Draw! No point awarded!`);
+        alert(`Your opponent selected ${computerSelection} as well. Draw! No point awarded!`);
+    }
+ }
+  
+ // playRound();
+ 
