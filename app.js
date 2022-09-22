cf6ef301898602396750a07c314d9c92264011aa
Travless <travis.white0101@gmail.com>
2022-09-22 14:25:09 -0400

    first draft with UI functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let playerScore = 0;
 let computerScore = 0;
+let outcome;
+let gameResults;
+// let rockInputs = ['Rock', 'rock']
 
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random()* 3) + 1
@@ -16,77 +19,129 @@ function getComputerChoice() {
 }
 // console.log(getComputerChoice());
 
-function playRound() {
-    let playersSelect = prompt('Please make your choice: Rock, Paper, or Scissors? ');
+function playRound(choice) {
+    let playersSelect = choice;
     let computerSelection = getComputerChoice();
     if (playersSelect === 'Rock' && computerSelection === 'Scissors') {
-        console.log(`Your opponent picked ${computerSelection}. You win! Rock beats Scissors!`);
-        alert(`Your opponent picked ${computerSelection}. You win! Rock beats Scissors!`);
+        outcome = `Your opponent picked ${computerSelection}. You win! Rock beats Scissors!`;
         playerScore += 1;
-        return playerScore;
+        return outcome && playerScore;
     } else if (playersSelect === 'Paper' && computerSelection === 'Rock') {
-        console.log(`Your opponent selected ${computerSelection}. You win! Paper beats Rock!`);
-        alert(`Your opponent selected ${computerSelection}. You win! Paper beats Rock!`);
         playerScore += 1;
-        return playerScore;
+        outcome = `Your opponent selected ${computerSelection}. You win! Paper beats Rock!`
+        return outcome && playerScore;
     } else if (playersSelect === 'Scissors' && computerSelection === 'Paper') {
-        console.log(`Your opponent selected ${computerSelection}. You win! Scissors beats Paper!`);
-        alert(`Your opponent selected ${computerSelection}. You win! Scissors beats Paper!`);
+        outcome = `Your opponent selected ${computerSelection}. You win! Scissors beats Paper!`;
         playerScore += 1;
-        return playerScore;
+        return outcome && playerScore;
     } else if (playersSelect === 'Scissors' && computerSelection === 'Rock') {
-        console.log(`Your opponent selected ${computerSelection}. You lose! Rock beats Scissors!`);
-        alert(`Your opponent selected ${computerSelection}. You lose! Rock beats Scissors!`);
+        outcome = `Your opponent selected ${computerSelection}. You lose! Rock beats Scissors!`;
         computerScore += 1;
-        return computerScore;
+        return outcome && computerScore;
     } else if (playersSelect === 'Rock' && computerSelection === 'Paper') {
-        console.log(`Your opponent selected ${computerSelection}. You lose! Paper beats Rock!`);
-        alert(`Your opponent selected ${computerSelection}. You lose! Paper beats Rock!`);
+        outcome = `Your opponent selected ${computerSelection}. You lose! Paper beats Rock!`;
         computerScore += 1;
-        return computerScore;
+        return outcome && computerScore;
     } else if (playersSelect === 'Paper' && computerSelection === 'Scissors') {
-        console.log(`Your opponent selected ${computerSelection}. You lose! Scissors beats Paper!`);
-        alert(`Your opponent selected ${computerSelection}. You lose! Scissors beats Paper!`);
+        outcome = `Your opponent selected ${computerSelection}. You lose! Scissors beats Paper!`;
         computerScore += 1;
-        return computerScore;
+        return outcome && computerScore;
     } else if (playersSelect === computerSelection) {
-        alert(`Your opponent selected ${computerSelection} as well. Draw! No point awarded!`);
-        console.log(`Your opponent selected ${computerSelection} as well. Draw! No point awarded!`);
+        outcome = `Your opponent selected ${computerSelection} as well. Draw! No point awarded!`;
+        return outcome;
     } else {
-        alert(`${playersSelect} is not a valid answer. No points awarded.`);
-        console.log(`${playersSelect} is not a valid answer. No points awarded.`);
+        outcome = `${playersSelect} is not a valid answer. No points awarded.`;
+        return outcome;
     }
- }
+ };
   
- // playRound();
- 
- function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        alert(`Current score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}`);
-        console.log(`Current score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}`);
-    }
-    if (playerScore > computerScore) {
-        alert(`Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. You win!`);
-        console.log(`Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. You win!`);
-    } else if (playerScore < computerScore) {
-        alert(`Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. You lose!`);
-        console.log(`Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. You lose!`);
+
+
+const resultsContainer = document.querySelector('#results-container');
+
+let currentResults = document.createElement('div');
+currentResults.setAttribute('id', 'current-results');
+resultsContainer.appendChild(currentResults);
+
+const runningScoreContainer = document.querySelector('#running-score-container');
+
+let runningScore = document.createElement('div');
+runningScore.setAttribute('id', 'running-score');
+runningScoreContainer.appendChild(runningScore);
+
+let endGame = document.createElement('div');
+endGame.setAttribute('id', 'game-end');
+runningScoreContainer.appendChild(endGame);
+
+function gameEnd() {
+    if (playerScore === 5 && computerScore < 5) {
+        gameResults = `Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. You win!`;
+        return gameResults;
+    } else if (playerScore < 5 && computerScore === 5) {
+        gameResults = `Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. You lose!`;
+        return gameResults;
     } else {
-        alert(`Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. Tie Game!`);
-        console.log(`Final Score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}. Tie Game!`);
     }
-    let replayGame = prompt('Would you like to play again? Please enter Yes or No.');
-    if (replayGame === 'Yes') {
-        game();
-    } else if (replayGame === 'No') {
-        alert('Thank you for playing!');
-        console.log('Thank you for playing!');
+};
+
+function disableBtns() {
+    if (playerScore === 5 && computerScore < 5) {
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+    } else if (playerScore < 5 && computerScore === 5) {
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
     } else {
-        alert('Invalid Answer. Please enter Yes or No.');
-        console.log('Invalid Answer. Please enter Yes or No.');
-        replayGame;
+        rockBtn.disabled = false;
+        paperBtn.disabled = false;
+        scissorsBtn.disabled = false;
     }
- }
- 
- game();
+}
+
+
+
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', function(e) {
+    playRound('Rock');
+    currentResults.textContent = outcome;
+    runningScore.textContent = `Current score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}`;
+    gameEnd();
+    endGame.textContent = gameResults;
+    disableBtns();
+});
+
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', function(e) {
+    playRound('Paper');
+    currentResults.textContent = outcome;
+    runningScore.textContent = `Current score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}`;
+    gameEnd();
+    endGame.textContent = gameResults;
+    disableBtns();
+});
+
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', function(e) {
+    playRound('Scissors');
+    currentResults.textContent = outcome;
+    runningScore.textContent = `Current score: Player's Score = ${playerScore}, Opponent's Score = ${computerScore}`;
+    gameEnd();
+    endGame.textContent = gameResults;
+    disableBtns();
+});
+
+
+
+// let replayGame = prompt('Would you like to play again? Please enter Yes or No.');
+// if (replayGame === 'Yes') {
+//     game();
+// } else if (replayGame === 'No') {
+//     alert('Thank you for playing!');
+//     console.log('Thank you for playing!');
+// } else {
+//     alert('Invalid Answer. Please enter Yes or No.');
+//     console.log('Invalid Answer. Please enter Yes or No.');
+//     replayGame;
+// }
